6908163162d9c33b966da617ebcefe4cef6f657d
mtang24 <michaeljtang18@gmail.com>
2025-02-14 10:27:02 -0800

    Add initial plot

--- global.js
+++ global.js
@@ -36,10 +36,49 @@ async function loadData() {
         m13_act: +row.m13_act,
       }));
     
-      console.log("Data Loaded:", data);
+     
       console.log(data[0]); // Logs first row
-      console.log(typeof data[0].f1_temp); // Should be "number"
+      console.log(data[0].f1_temp); // Should be "number"
       console.log(typeof data[0].Day); // Should be "number"
+
+      plotData();
+}
+
+function plotData() {
+    const margin = { top: 20, right: 30, bottom: 40, left: 50 };
+    const width = 800 - margin.left - margin.right;
+    const height = 400 - margin.top - margin.bottom;
+
+    const svg = d3.select("body").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    const x = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.Day)])
+        .range([0, width]);
+
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.f1_temp)])
+        .range([height, 0]);
+
+    svg.append("g")
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisBottom(x));
+
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
+    svg.append("path")
+        .datum(data)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr("d", d3.line()
+            .x(d => x(d.Day))
+            .y(d => y(d.f1_temp))
+        );
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
